4bbff6b766b13ead58d8e28c0ffe7466a7c1bd4b
LIKImk <103368018+theLIKImk@users.noreply.github.com>
2025-09-04 20:44:24 +0800

    Add files via upload

---
+++ example-rely_on.bat
@@ -0,0 +1,29 @@
+@echo off
+::v1.0.0
+IF /I "%1"=="/SHOW" GOTO :SHOW 
+
+set PID_START_PATH_SET=%~nx0 /SHOW Main_form
+call pid /start solo cmd
+
+
+set PIDMD_RELY_ON=%errorlevel%
+
+set PID_START_PATH_SET=%~nx0 /SHOW Subform_MAIN:%PIDMD_RELY_ON%
+call pid /start SOLO cmd
+
+set getpid=%errorlevel%
+set PID_START_PATH_SET=%~nx0 /SHOW Subform:%getpid%
+call pid /start %getpid% cmd
+set PID_START_PATH_SET=%~nx0 /SHOW Subform:%getpid%
+call pid /start %getpid% cmd
+
+EXIT /B
+
+:SHOW
+	title [%PIDMD_PRID%]MSG
+	ECHO [%2]
+	ECHO.
+	ECHO.PID VER
+	CALL PID /VERSION
+	PAUSE
+	EXIT
